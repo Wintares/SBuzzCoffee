4348ed11befcb6f6408dde3bc3b58a0caaf6c5da
Wintares <spellcraftung@gmail.com>
2017-03-25 17:19:49 +0300

    What

---
+++ Starbuzz/app/src/main/java/com/kulibin/artur/starbuzz/Drink.java
@@ -0,0 +1,43 @@
+package com.kulibin.artur.starbuzz;
+
+/**
+ * Created by Artur on 24.03.2017.
+ */
+
+public class Drink {
+
+    private String name;
+    private String description;
+    private int imageResourceId;
+    //drinks - array with Drink elements
+    public static final Drink[] drinks = {
+            new Drink("Latte","A couple of espresso shots with steamed milk", R.drawable.latte),
+            new Drink("Cappuccino","Espresso, hit milk, and a steamed milk foam", R.drawable.cappuccino),
+            new Drink("Filter", "Highest quality beans roasted and brewed fresh", R.drawable.filter)
+    };
+
+
+
+
+    private Drink(String name, String description, int imageResourceId){
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
---
+++ Starbuzz/app/src/main/java/com/kulibin/artur/starbuzz/DrinkActivity.java
@@ -0,0 +1,30 @@
+package com.kulibin.artur.starbuzz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DrinkActivity extends AppCompatActivity {
+    public static final String EXTRA_DRINKNO = "drinkNo";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+        //Get drink number from intent nIGA
+        int drinkNo = (Integer)getIntent().getExtras().get(EXTRA_DRINKNO);
+        Drink drink = Drink.drinks[drinkNo];
+        //Fill image for current Drink
+        ImageView photo = (ImageView)findViewById(R.id.photo);
+        photo.setImageResource(drink.getImageResourceId());
+        photo.setContentDescription(drink.getName());
+        //Fill drink name
+        TextView name = (TextView)findViewById(R.id.name);
+        name.setText(drink.getName());
+        //Fill Drink Description
+        TextView description = (TextView)findViewById(R.id.description);
+        description.setText(drink.getDescription());
+
+    }
+}
